931255a40260e9713d61781cb46e64389a438b93
Ricardo Ferrancullo III <ricardo.ferrancullo@kotahi.co.nz>
2021-12-17 11:36:14 +1300

    Updates

---
+++ precache-manifest.454b3e561e5b743f44f545f295046f19.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866bdb00548d3407f3741893d06b1aae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef743c5e79ad8edf900",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "423e9ce4cf9c6f4c6a98",
+    "url": "/static/js/2.dd010ef6.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.dd010ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef743c5e79ad8edf900",
+    "url": "/static/js/main.dc8e71e3.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8671ea86b01ab70005ceb68d5869f16e",
+    "url": "/static/media/azure-developer-associate.8671ea86.png"
+  },
+  {
+    "revision": "0902f056bdae83256db745bfc66a174e",
+    "url": "/static/media/azure-devops.0902f056.png"
+  },
+  {
+    "revision": "76c0becf68517d645aea06e321438af4",
+    "url": "/static/media/azure-fundamentals.76c0becf.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "771aafb22bb2d2d507b01982ce720401",
+    "url": "/static/media/kinpo.771aafb2.jpg"
+  },
+  {
+    "revision": "bb04964860fc766c0338936fb69973a2",
+    "url": "/static/media/maybank.bb049648.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "effb1fdcc5d14142710fcef0c74e1d31",
+    "url": "/static/media/ricthethird.effb1fdc.jpg"
+  }
+]);
